86ad7290df9f893f0ce217486798cb099ed5ea4d
Moto-Git <cubebz11h16@gmail.com>
2020-09-15 04:16:19 +0900

    migration study

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,36 @@
 class Task < ApplicationRecord
+
+  before_validation :set_nameless_name
+
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+
+  # 短く書くのであれば 
+  # validates :name, presence: true, length: { maximum: 30 }
+  # とできる
+
+  # test
+  # validate :validate_name_not_including_comma
+  
+  # private
+
+  # def validate_name_not_including_comma
+  #   errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  # end
+
+  validate :validate_name_not_including_comma
+
+  private
+
+  def set_nameless_name
+    self.name = '名前無し' if name.blank?
+  end
+
+
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません。') if name&.include?(',')
+  end
+
 end
